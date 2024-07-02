0b2ef79621534493b9ca3527914b3da9bad04157
wiseai-mead <mead@wiseai.co.kr>
2024-07-02 13:56:28 +0900

    init

---
+++ src/main/kotlin/project/zzzz/ZzzzApplication.kt
@@ -0,0 +1,11 @@
+package project.zzzz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZzzzApplication
+
+fun main(args: Array<String>) {
+  runApplication<ZzzzApplication>(*args)
+}
---
+++ src/test/kotlin/project/zzzz/ZzzzApplicationTests.kt
@@ -0,0 +1,13 @@
+package project.zzzz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZzzzApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
